b069b988deb38ae7c3c9fdd49aed856c7def10f0
Harshith Sunku <harshithsunku@gmail.com>
2024-08-04 22:40:21 +0530

    added comments to kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -3,20 +3,64 @@
 #include <stdint.h>
 
 
+/**
+ * Pointer to the video memory.
+ */
 uint16_t* video_mem = 0;
+
+/**
+ * Current row of the terminal.
+ */
 uint16_t terminal_row = 0;
+
+/**
+ * Current column of the terminal.
+ */
 uint16_t terminal_col = 0;
 
+/**
+ * Create a character with the given foreground and background colours.
+ *
+ * @param c The ASCII character to create.
+ * @param colour The colour of the character.
+ *
+ * @return The created character.
+ */
 uint16_t terminal_make_char(char c, char colour)
 {
+    /* Shift the colour to the left by 8 bits and OR it with the character.
+     * This effectively creates a 16-bit integer where the top 8 bits are the
+     * colour and the bottom 8 bits are the character. */
     return (colour << 8) | c;
 }
 
+/**
+ * Writes a character to the terminal at the specified coordinates.
+ *
+ * @param x The x-coordinate of the character.
+ * @param y The y-coordinate of the character.
+ * @param c The character to write.
+ * @param colour The colour of the character.
+ *
+ * @return void
+ *
+ * @throws None
+ */
 void terminal_putchar(int x, int y, char c, char colour)
 {
     video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
 }
 
+/**
+ * Writes a character to the terminal at the specified coordinates.
+ *
+ * @param c The character to write.
+ * @param colour The colour of the character.
+ *
+ * @return void
+ *
+ * @throws None
+ */
 void terminal_writechar(char c, char colour)
 {
     if (c == '\n')
@@ -34,6 +78,11 @@ void terminal_writechar(char c, char colour)
         terminal_row += 1;
     }
 }
+/**
+ * Initializes the terminal by setting the video memory address and clearing the screen.
+ *
+ * @return void
+ */
 void terminal_initialize()
 {
     video_mem = (uint16_t*)(0xB8000);
@@ -49,6 +98,15 @@ void terminal_initialize()
 }
 
 
+/**
+ * Calculates the length of a null-terminated string.
+ *
+ * @param str Pointer to the string.
+ *
+ * @return The length of the string.
+ *
+ * @throws None
+ */
 size_t strlen(const char* str)
 {
     size_t len = 0;
@@ -60,6 +118,15 @@ size_t strlen(const char* str)
     return len;
 }
 
+/**
+ * Prints the given string to the terminal.
+ *
+ * @param str Pointer to the string to be printed.
+ *
+ * @return void
+ *
+ * @throws None
+ */
 void print(const char* str)
 {
     size_t len = strlen(str);
@@ -69,6 +136,11 @@ void print(const char* str)
     }
 }
 
+/**
+ * Initializes the terminal and prints a greeting message.
+ *
+ * @return void
+ */
 void kernel_main()
 {
     terminal_initialize();
